1709a2a08f8a01a1f745765f805f7184b0a69070
SknightS <md.sakibrahman@gmail.com>
2017-04-02 19:00:40 +0600

    Sakib

--- application/views/admin_home.php
+++ application/views/admin_home.php
@@ -36,6 +36,32 @@
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
     <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
 
+
+    <style>
+        ::-webkit-input-placeholder {
+            color: none;
+        }
+
+        :-moz-placeholder { /* Firefox 18- */
+            color: red;
+        }
+
+        ::-moz-placeholder {  /* Firefox 19+ */
+            color: red;
+        }
+
+        :-ms-input-placeholder {
+            color: red;
+        }
+
+        .form-control {
+            background-image: none;
+        }
+        .form-group {
+            background-image: none;
+        }
+    </style>
+
     <!-- include summernote css/js-->
     <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
     <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
